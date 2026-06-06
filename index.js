746ad0547f7c713f318979c0fdd0a05f18f938b1
soydegou <degoboy.github.io>
2026-06-06 04:05:31 -0400

    Preparando para despliegue web

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import { registerRootComponent } from 'expo';
-
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// registerRootComponent llama a AppRegistry.registerComponent('main', () => App);
+// También garantiza que, ya sea que cargue la aplicación en Expo Go o en una compilación nativa,
+// el entorno está configurado adecuadamente.
 registerRootComponent(App);
